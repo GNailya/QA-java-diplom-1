569179311427c1255728d466f5cc178cdcd29f72
Nailya <nelya.gabdulkhakova@mail.ru>
2022-08-21 03:00:23 +0500

    Добавлены тесты на классы Bun, Burger, Ingredient, IngredientType

--- src/test/java/BunTest.java
+++ src/test/java/BunTest.java
@@ -1,2 +1,21 @@
-package PACKAGE_NAME;public class BunTest {
+import org.junit.Test;
+import praktikum.Bun;
+
+import static org.junit.Assert.assertEquals;
+
+public class BunTest {
+    String name = "Test bun";
+    float price = 100;
+
+    Bun bun = new Bun(name, price);
+
+    @Test
+    public void getBunNameTest() {
+        assertEquals(name, bun.getName());
+    }
+
+    @Test
+    public void getBunPriceTest() {
+       assertEquals(price, bun.getPrice(), 0);
+    }
 }
--- src/test/java/IngredientTypeTest.java
+++ src/test/java/IngredientTypeTest.java
@@ -1,2 +1,35 @@
-package PACKAGE_NAME;public class IngredientTypeTest {
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import praktikum.Ingredient;
+import praktikum.IngredientType;
+
+import static org.junit.Assert.assertEquals;
+import static praktikum.IngredientType.FILLING;
+import static praktikum.IngredientType.SAUCE;
+
+@RunWith(Parameterized.class)
+public class IngredientTypeTest {
+    private final IngredientType type;
+    private final IngredientType expectedType;
+
+    public IngredientTypeTest(IngredientType type, IngredientType expectedType) {
+        this.type = type;
+        this.expectedType = expectedType;
+    }
+
+    @Parameterized.Parameters
+    public static Object[][] getTestData() {
+        return new Object[][]{
+                {SAUCE, SAUCE},
+                {FILLING, FILLING},
+        };
+    }
+
+    @Test
+    public void getType() {
+        Ingredient ingredient = new Ingredient(type, "Начинка",100);
+        IngredientType actual = ingredient.getType();
+        assertEquals(expectedType,actual) ;
+    }
 }
